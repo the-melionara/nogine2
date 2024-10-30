be85fe704e3ea191deb358fe32edfb8d19f99ebd
Mel <yowax25yt@gmail.com>
2024-10-30 19:36:09 +0100

    Updated drawing example

--- nogine2/examples/drawing.rs
+++ nogine2/examples/drawing.rs
@@ -1,4 +1,4 @@
-use nogine2::{colors::{rgba::RGBA32, Color}, graphics::CameraData, math::vector2::{uvec2, vec2}, prelude::init_nogine2, window::{Window, WindowCfg}};
+use nogine2::{colors::{rgba::RGBA32, Color}, graphics::{CameraData, Graphics}, input::{keyboard::Key, Input}, math::vector2::{uvec2, vec2}, prelude::init_nogine2, window::{Window, WindowCfg}};
 
 fn main() {
     init_nogine2();
@@ -6,8 +6,19 @@ fn main() {
     let mut window = Window::new(WindowCfg { title: "Nogine 2", res: uvec2(1280, 720) });
     window.set_vsync(true);
 
+    let mut center = vec2::ZERO;
     while window.is_open() {
-        window.pre_tick(CameraData { center: vec2::ZERO, extents: vec2(window.aspect_ratio(), 1.0) * 2.0 }, window.res(), RGBA32::BLACK, None);
+        center += vec2::from(Input::keyboard().axis2((Key::A, Key::S), (Key::D, Key::W))) * window.ts();
+        window.pre_tick(CameraData { center, extents: vec2(window.aspect_ratio(), 1.0) * 5.0 }, window.res(), RGBA32::BLACK, None);
+
+        Graphics::draw_rect(vec2(-1.0, -1.0), 0.0, vec2::one(2.0), RGBA32::WHITE);
+
+        Graphics::draw_rect(vec2(-1.5, -0.75), 0.0, vec2::one(1.5), RGBA32::YELLOW);
+        Graphics::draw_rect(vec2( 0.0, -0.75), 0.0, vec2::one(1.5), RGBA32::CYAN);
+
+        Graphics::draw_rect(vec2(-2.0, -0.5), 0.0, vec2::ONE, RGBA32::RED);
+        Graphics::draw_rect(vec2(-0.5, -0.5), 0.0, vec2::ONE, RGBA32::GREEN);
+        Graphics::draw_rect(vec2( 1.0, -0.5), 0.0, vec2::ONE, RGBA32::BLUE);
 
         window.post_tick();
     }
