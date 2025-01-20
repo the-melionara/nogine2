3717a1210a63226d128794c8c925ab976dc81cd0
Mel <yowax25yt@gmail.com>
2025-01-20 22:34:58 +0100

    added cfg to fix bleeding on non-pixel perfect contexts

--- nogine2-graphics/src/graphics/texture/sprite.rs
+++ nogine2-graphics/src/graphics/texture/sprite.rs
@@ -7,13 +7,14 @@ use super::{Texture2D, TextureHandle};
 pub struct SpriteAtlas {
     tex: Texture2D,
     cell_size: uvec2,
+    epsilon: AtlasEpsilonMode,
 }
 
 impl SpriteAtlas {
     /// Creates a new `SpriteAtlas`. Will panic if `cell_size.0 == 0` or `cell_size.1 == 0`.
     pub fn new(texture: Texture2D, cell_size: uvec2) -> Self {
         assert_expr!(cell_size.0 > 0 && cell_size.1 > 0, "'cell_size' must be greater than 0 for every axis!");
-        return Self { tex: texture, cell_size };
+        return Self { tex: texture, cell_size, epsilon: AtlasEpsilonMode::None };
     }
 
     pub fn tex(&self) -> &Texture2D {
@@ -35,19 +36,26 @@ impl SpriteAtlas {
         return self;
     }
 
+    pub fn set_epsilon_mode(&mut self, epsilon: AtlasEpsilonMode) {
+        self.epsilon = epsilon;
+    }
+
     /// Samples a sprite from a `rect` in cell space. Will panic if `rect.start <= rect.end` for any axis.
     pub fn get(&self, rect: IRect) -> Sprite {
         assert_expr!(rect.start.0 < rect.end.0 && rect.start.1 < rect.end.1, "'rect.start' must be lesser than 'rect.end' for every axis!");
 
+        let start_texel_offset = self.epsilon.start_offset();
+        let end_texel_offset = self.epsilon.end_offset();
+        
         let cell_count = self.tex.dims().inv_scale(self.cell_size);
         let uv_rect = Rect {
             start: (vec2::from(rect.start)
                 .inv_scale(vec2::from(cell_count))
-                .scale(vec2::from(self.tex.dims())) + vec2::one(0.1))
+                .scale(vec2::from(self.tex.dims())) + vec2::one(start_texel_offset))
                 .inv_scale(vec2::from(self.tex.dims())),
             end: (vec2::from(rect.end)
                 .inv_scale(vec2::from(cell_count))
-                .scale(vec2::from(self.tex.dims())) + vec2::one(0.1))
+                .scale(vec2::from(self.tex.dims())) + vec2::one(end_texel_offset))
                 .inv_scale(vec2::from(self.tex.dims())),
         };
         return Sprite { handle: self.tex.handle(), uv_rect };
@@ -73,5 +81,35 @@ impl Sprite {
 
     pub fn dims(&self) -> uvec2 {
         uvec2::from(vec2::from(self.handle.dims()).scale(self.uv_rect.size()))
+    }
+}
+
+/// Defines how the atlas will add margins and shift the UVs to avoid visual artifacts.
+#[derive(Debug, Clone, Copy)]
+pub enum AtlasEpsilonMode {
+    None,
+    /// Fit for pixel perfect rendering.
+    PixelPerfect,
+    /// Fir for not pixel perfect rendering where tiles bleed into each other. `texels` is measured in texture pixels.
+    Shrinking { texels: f32 },
+}
+
+impl AtlasEpsilonMode {
+    const PIXEL_PERFECT_TEXEL: f32 = 0.1;
+    
+    fn start_offset(&self) -> f32 {
+        match self {
+            AtlasEpsilonMode::None => 0.0,
+            AtlasEpsilonMode::PixelPerfect => Self::PIXEL_PERFECT_TEXEL,
+            AtlasEpsilonMode::Shrinking { texels } => *texels,
+        }
+    }
+
+    fn end_offset(&self) -> f32 {
+        match self {
+            AtlasEpsilonMode::None => 0.0,
+            AtlasEpsilonMode::PixelPerfect => Self::PIXEL_PERFECT_TEXEL,
+            AtlasEpsilonMode::Shrinking { texels } => -(*texels),
+        }
     }
 }
