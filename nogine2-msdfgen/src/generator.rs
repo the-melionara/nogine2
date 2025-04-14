82429f291d0e2a2e8f5f62a81452e26b9ce1d676
Mel <yowax25yt@gmail.com>
2025-04-14 23:23:31 +0200

    bunch of math pain

--- nogine2-msdfgen/src/generator.rs
+++ nogine2-msdfgen/src/generator.rs
@@ -127,33 +127,133 @@ impl OutlineBuilder for ContourIterator {
     }
 }
 
-// fn cubic_bezier(a: vec2, b: vec2, c: vec2, d: vec2, t: f32) -> vec2 {
-//     let ab = a.lerp(b, t);
-//     let bc = b.lerp(c, t);
-//     let cd = c.lerp(d, t);
+/// See (2.23) and (2.24)
+fn is_smooth_corner(dadt_1: vec2, dbdt_0: vec2) -> bool {
+    const ALPHA: f32 = 1.0;
 
-//     let ac = ab.lerp(bc, t);
-//     let bd = bc.lerp(cd, t);
+    return vec2_cross(dadt_1.normalized(), dbdt_0.normalized()).abs() < ALPHA.sin() &&
+        dadt_1.dot(dbdt_0) > 0.0;
+}
 
-//     return ac.lerp(bd, t);
-// }
-
-/// As defined by the paper
+/// See (2.15)
 fn vec2_cross(a: vec2, b: vec2) -> f32 {
     a.0 * b.1 - a.1 * b.0
 }
 
+/// See (2.43)
+fn orthogonality(b_t: vec2, dbdt_t: vec2, p: vec2) -> f32 {
+    vec2_cross(dbdt_t.normalized(), (p - b_t).normalized()).abs()
+}
+
+/// See (2.41)
+fn sdistance(b_t: vec2, dbdt_t: vec2, p: vec2) -> f32 {
+    return vec2_cross(dbdt_t, b_t - p).signum() * (b_t - p).magnitude();
+}
+
+/// See (2.28)
+fn dist_to_line(p: vec2, p0: vec2, p1: vec2) -> f32 {
+    let t = ((p - p0).dot(p1 - p0) / (p1 - p0).dot(p1 - p0)).clamp(0.0, 1.0);
+    return line(p0, p1, t).dist_to(p);
+}
+
+/// See (2.38)
+fn dist_to_bezier2(p: vec2, p0: vec2, p1: vec2, p2: vec2) -> f32 {
+    let pv = p - p0;
+    let pv1 = p1 - p0;
+    let pv2 = p2 - p1 * 2.0 + p0;
+    //let pv3 = p3 - p2 * 3.0 + p1 * 3.0 - p0;
+    
+    let t = cardano( // TODO: ACTUALLY USE ALL ROOTS
+        pv2.dot(pv2),
+        pv1.dot(pv2) * 3.0,
+        pv1.dot(pv1) * 2.0 - pv2.dot(pv),
+        pv1.dot(pv),
+    ).clamp(0.0, 1.0);
+    return bezier2(p0, p1, p2, t).dist_to(p);
+}
+
+/// See (2.40)
+fn dist_to_bezier3(p: vec2, p0: vec2, p1: vec2, p2: vec2, p3: vec2) -> f32 {
+    let pv = p - p0;
+    let pv1 = p1 - p0;
+    let pv2 = p2 - p1 * 2.0 + p0;
+    let pv3 = p3 - p2 * 3.0 + p1 * 3.0 - p0;
+    
+    let t = quintic_aproximation( // TODO: ACTUALLY USE ALL ROOTS
+        pv3.dot(pv3),
+        pv2.dot(pv3) * 5.0,
+        pv1.dot(pv3) * 4.0 + pv2.dot(pv2) * 6.0,
+        pv1.dot(pv2) * 9.0 - pv2.dot(pv),
+        pv1.dot(pv1) * 3.0 - pv2.dot(pv) * 2.0,
+        pv1.dot(pv),
+    ).clamp(0.0, 1.0);
+    return bezier3(p0, p1, p2, p3, t).dist_to(p);
+}
+
+/// Not really only cardano because it also depresses it but whatever
+fn cardano(a: f32, b: f32, c: f32, d: f32) -> f32 {
+    // I still hate maths
+    let p = (3.0 * a * c - b * b) / (3.0 * a * a);
+    let q = (2.0 * b * b * b - 9.0 * a * b * c + 27.0 * a * a * d) / (27.0 * a * a * a);
+
+    let delta = (q * 0.5) * (q * 0.5) + (p / 3.0) * (p / 3.0) * (p / 3.0);
+    let x = (-q * 0.5 + (delta).sqrt()).powf(1.0 / 3.0) +
+        (-q * 0.5 - (delta).sqrt()).powf(1.0 / 3.0);
+
+    return x - b / (3.0 * a);
+}
+
+/// Done with newthon's method. Maybe there's something better, maybe not.
+fn quintic_aproximation(a: f32, b: f32, c: f32, d: f32, e: f32, f: f32) -> f32 {
+    fn g(x: f32, a: f32, b: f32, c: f32, d: f32, e: f32, f: f32) -> f32 {
+        let x2 = x * x;
+        let x3 = x2 * x;
+        let x4 = x3 * x;
+        let x5 = x4 * x;
+
+        return a * x5 + b * x4 + c * x3 + d * x2 + e * x + f;
+    }
+
+    fn dgdx(x: f32, a: f32, b: f32, c: f32, d: f32, e: f32) -> f32 {
+        let x2 = x * x;
+        let x3 = x2 * x;
+        let x4 = x3 * x;
+
+        return 5.0 * a * x4 + 4.0 * b * x3 + 3.0 * c * x2 + 2.0 * d * x + e;
+    }
+
+    let mut x = 0.5;
+    for _ in 0..10 { // 10 iterations, take it or leave it
+        let gx = g(x, a, b, c, d, e, f);
+        if gx.abs() < f32::EPSILON {
+            break;
+        }
+        x = x - gx / dgdx(x, a, b, c, d, e);
+    }
+    return x;
+}
+
+fn line(a: vec2, b: vec2, t: f32) -> vec2 {
+    return a * (-t + 1.0) +
+        b * t;
+}
+
+fn line_dt(a: vec2, b: vec2) -> vec2 {
+    return b - a;
+}
+
 fn bezier2(a: vec2, b: vec2, c: vec2, t: f32) -> vec2 {
     let t2 = t * t;
-    return (c - a) * t2 + (b - a) * 2.0 * t + a;
+    return a * (-t2 + 2.0 * t - 1.0) +
+        b * (2.0 * t) +
+        c * t2;
 }
 
 fn bezier2_dt(a: vec2, b: vec2, c: vec2, t: f32) -> vec2 {
-    let ab = a.lerp(b, t);
-    let bc = b.lerp(c, t);
-    return ab.lerp(bc, t);
+    return a * (-2.0 * t + 2.0) +
+        b * 2.0 +
+        c * (2.0 * t);
 }
-
 
 fn bezier3(a: vec2, b: vec2, c: vec2, d: vec2, t: f32) -> vec2 {
     // Ripped straight from Freya Holmér's video on beziers (bless her heart)
