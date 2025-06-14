1da1deb6430c00913a5b4ac5520a975025f165e7
Mel <yowax25yt@gmail.com>
2025-06-14 22:32:04 +0200

    Added new handy methods to Rects

--- nogine2-math/src/rect.rs
+++ nogine2-math/src/rect.rs
@@ -11,3 +11,28 @@ float_rect_impl!(Rect, vec2);
 
 gen_rect!(DRect, dvec2, f64, 2.0);
 float_rect_impl!(DRect, dvec2);
+
+
+impl IRect {
+    pub fn from_points(a: ivec2, b: ivec2) -> Self {
+        Self { start: a.min(b), end: a.max(b) }
+    }
+}
+
+impl URect {
+    pub fn from_points(a: uvec2, b: uvec2) -> Self {
+        Self { start: a.min(b), end: a.max(b) }
+    }
+}
+
+impl Rect {
+    pub fn from_points(a: vec2, b: vec2) -> Self {
+        Self { start: a.min(b), end: a.max(b) }
+    }
+}
+
+impl DRect {
+    pub fn from_points(a: dvec2, b: dvec2) -> Self {
+        Self { start: a.min(b), end: a.max(b) }
+    }
+}
