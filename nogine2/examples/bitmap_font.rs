c40918e2b5d5577eee1589b3efa87ee7e9dfdbb5
Mel <yowax25yt@gmail.com>
2025-06-24 03:43:54 +0200

    added ruler to example

--- nogine2/examples/bitmap_font.rs
+++ nogine2/examples/bitmap_font.rs
@@ -1,6 +1,6 @@
 use std::{any::Any, fs::File};
 
-use nogine2::{colors::{rgba::RGBA32, Color}, graphics::{text::{align::HorTextAlign, font::{BitmapFont, FontCfg, Measure, TextStyle}, TextCfg}, texture::{sprite::SpriteAtlas, Texture2D, TextureFiltering, TextureSampling, TextureWrapping}, CameraData, FrameSetup, Graphics}, input::{keyboard::Key, Input}, math::{rect::Rect, vector2::{uvec2, vec2}}, prelude::init_nogine2, unwrap_res, window::{Window, WindowCfg}};
+use nogine2::{colors::{rgba::RGBA32, Color}, graphics::{gfx::screen_to_world_pos, text::{align::HorTextAlign, font::{BitmapFont, FontCfg, Measure, TextStyle}, TextCfg}, texture::{sprite::SpriteAtlas, Texture2D, TextureFiltering, TextureSampling, TextureWrapping}, CameraData, FrameSetup, Graphics}, input::{keyboard::Key, mouse::Button, Input}, math::{rect::Rect, vector2::{uvec2, vec2}}, prelude::init_nogine2, unwrap_res, window::{Window, WindowCfg}};
 
 fn main() {
     init_nogine2();
@@ -27,6 +27,8 @@ fn main() {
 
     let mut text_pos = vec2::ZERO;
     let mut width = 6.0;
+    let mut ruler_origin = None;
+    
     while window.is_open() {
         window.pre_tick(FrameSetup {
             camera: CameraData {
@@ -59,6 +61,25 @@ fn main() {
 
             "DELTARUNE TOMORROW REAL NO FAKE 1 LINK MEDIAFIRE"
         );
+
+        match ruler_origin {
+            Some(origin) => {
+                Graphics::draw_line(
+                    origin,
+                    screen_to_world_pos(Input::mouse().pos()),
+                    [RGBA32::RED; 2],
+                );
+
+                if Input::mouse().button_released(Button::Left) {
+                    ruler_origin = None;
+                }
+            },
+            None => {
+                if Input::mouse().button_pressed(Button::Left) {
+                    ruler_origin = Some(screen_to_world_pos(Input::mouse().pos()));
+                }
+            },
+        }
 
         dbg!(window.post_tick());
     }
