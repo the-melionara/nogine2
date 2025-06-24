08a7afeeab1d62ec4216a2e97cbb6d3e586051f7
Mel <yowax25yt@gmail.com>
2025-06-24 03:43:30 +0200

    added screen_to_world_pos utility function

--- nogine2-graphics/src/graphics/gfx.rs
+++ nogine2-graphics/src/graphics/gfx.rs
@@ -1,6 +1,6 @@
-use nogine2_core::math::{rect::IRect, vector2::ivec2};
+use nogine2_core::math::{rect::IRect, vector2::{ivec2, vec2}};
 
-use super::{pipeline::RenderStats, texture::rendertex::RenderTexture};
+use super::{pipeline::RenderStats, texture::rendertex::RenderTexture, Graphics};
 
 /// Transfer the data from `src` to `dst` retaining integer scaling for pixels.
 pub fn integer_scaling_blit(src: &RenderTexture, dst: &RenderTexture, stats: &mut RenderStats) {
@@ -18,3 +18,13 @@ mod integer_scaling {
         dst_res.inv_scale(src_res).min_axis().max(1)
     }
 }
+
+/// Converts screen space positions to world space positions.
+pub fn screen_to_world_pos(pos: vec2) -> vec2 {
+    let cam = Graphics::camera();
+    let half_res = vec2::from(Graphics::target_res()) * 0.5;
+
+    let unit_space = (pos.inv_scale(half_res) - vec2::ONE).scale(vec2(1.0, -1.0)); // -1 to 1
+    return unit_space.scale(cam.extents * 0.5) + cam.center;
+}
+
